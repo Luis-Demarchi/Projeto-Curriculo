bc8769ce7cc0152283f0218f742b8f10d156a3a6
Luis Demarchi <luisdemarchi3@gmail.com>
2025-03-19 17:19:25 -0300

    Added the finals adjustments

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,33 +1,57 @@
-class MobileNavbar{
-    constructor(mobileMenu, navList, navLinks){
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
         this.mobileMenu = document.querySelector(mobileMenu);
         this.navList = document.querySelector(navList);
         this.navLinks = document.querySelectorAll(navLinks);
         this.activeClass = "active";
 
         this.handleClick = this.handleClick.bind(this);
+        this.handleLinkClick = this.handleLinkClick.bind(this);
+        this.handleOutsideClick = this.handleOutsideClick.bind(this);
     }
 
-    animateLinks(){
+    animateLinks(opening) {
         this.navLinks.forEach((link, index) => {
-            link.style.animation
-            ? (link.style.animation = "")
-            : (link.style.animation = `navLinkFade 0.5 ease forwards ${index * 0.5 + 0.3}s`);
+            if (opening) {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index * 0.1 + 0.3}s`;
+            } else {
+                link.style.animation = "";
+            }
         });
     }
 
-    handleClick(){
+    handleClick() {
+        const isOpening = !this.navList.classList.contains(this.activeClass);
+
         this.navList.classList.toggle(this.activeClass);
         this.mobileMenu.classList.toggle(this.activeClass);
-        this.animateLinks();
+        this.animateLinks(isOpening);
     }
 
-    addClickEvent(){
+    handleLinkClick() {
+        this.navList.classList.remove(this.activeClass);
+        this.mobileMenu.classList.remove(this.activeClass);
+        this.animateLinks(false);
+    }
+
+    handleOutsideClick(event) {
+        if (!this.mobileMenu.contains(event.target) && !this.navList.contains(event.target)) {
+            this.navList.classList.remove(this.activeClass);
+            this.mobileMenu.classList.remove(this.activeClass);
+            this.animateLinks(false);
+        }
+    }
+
+    addClickEvent() {
         this.mobileMenu.addEventListener("click", this.handleClick);
+        this.navLinks.forEach(link => {
+            link.addEventListener("click", this.handleLinkClick);
+        });
+        document.addEventListener("click", this.handleOutsideClick);
     }
 
-    init(){
-        if(this.mobileMenu){
+    init() {
+        if (this.mobileMenu) {
             this.addClickEvent();
         }
         return this;
@@ -37,7 +61,7 @@ class MobileNavbar{
 const MobileNavbarInstance = new MobileNavbar(
     ".mobile-menu",
     ".nav-list",
-    ".nav-list li",
+    ".nav-list li"
 );
 MobileNavbarInstance.init();
 
